f9791f76335ca0ec4e818a6eb55165bfed634ee6
hraj4hr <hrbharti.tech@gmail.com>
2021-11-15 15:35:53 +0530

    fetch API performed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import List from './components/List';
+import WithListLoading from './components/WithListLoading';
 function App() {
+  const ListLoading = WithListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  const style = {
+    color: 'green',
+    fontSize: 80
+  };
+
+  const honorName = {
+    color: 'blue',
+    fontSize: 20
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-         Hi This is Hansaraj Bharati
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Developer...
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1 style={style}>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          with by   
+          <h1 style={honorName}>  Hansaraj Bharati</h1>
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
